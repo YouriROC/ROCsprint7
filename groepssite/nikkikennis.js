7ba7599d1e858df6722f86c20f01dc09fec87c97
[NikkivB] <[0316641@student.rocvantwente.nl]>
2020-02-29 15:04:33 +0100

    -

---
+++ groepssite/nikkikennis.js
@@ -0,0 +1,15 @@
+var vraag1 = "Wat is een Call to undefined function-error?";
+var vraag2 = "Wat is de phpinfo-function?";
+var vraag3 = "Waar kun je de Apache-server fouten traceren?";
+var vraag4 = "Wat is de root-map van de Apache-server?";
+var vraag5 = "Benoem alle error-logs van XAMPP.";
+var vraag6 = "Wat is een CMS?";
+var vraag7 = "Wat is Joomla?";
+var vraag8 = "Wat is syntaxis";
+var vraag9 = "Wat doet de echo-opdracht?";
+var vraag10 = "Waar kunnen we een PHP-script embedden in een html-script?";
+
+function kennisToets1() {
+    document.getElementsByClassName("kennistoets_antw").innerHTML +=
+    "<h2>Wat is een Call to undefined function-error?</h2>";
+}
