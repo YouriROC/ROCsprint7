247e79b1464421f3200620dc39991676a51ab153
[NikkivB] <[0316641@student.rocvantwente.nl]>
2020-03-02 14:55:16 +0100

    nikki kennislabspagina 0.3

--- groepssite/nikkikennis.js
+++ groepssite/nikkikennis.js
@@ -20,9 +20,31 @@ var antwoord1_8 = "Niet beantwoord";
 var antwoord1_9 = "Laat tekst zien";
 var antwoord1_10 = "In de head en onderaan de body";
 
+var vraag2_1 = "$auto2";
+var vraag2_2 = "$auto's";
+var vraag2_3 = "$auto/onderdelen";
+var vraag2_4 = "$thuis&tuin?";
+var vraag2_5 = "$naw";
+var vraag2_6 = "$n-a-w";
+var vraag2_7 = "$n_a_w";
+var vraag2_8 = "$22";
+var vraag2_9 = "$$auto";
+var vraag2_10 = "$autokleur";
 
+var antwoord2_1 = "ja";
+var antwoord2_2 = "nee";
+var antwoord2_3 = "nee";
+var antwoord2_4 = "nee";
+var antwoord2_5 = "ja";
+var antwoord2_6 = "nee";
+var antwoord2_7 = "ja";
+var antwoord2_8 = "ja";
+var antwoord2_9 = "nee";
+var antwoord2_10 = "ja";
 
 function kennis1() {
+    document.getElementById("titelKennis").innerHTML = "Kennistoets 1.02"
+
     document.getElementById("vraag1").innerHTML = "<strong>" + vraag1_1 + "</strong>";
     document.getElementById("antwoord1").innerHTML = antwoord1_1;
     document.getElementById("vraag2").innerHTML = "<strong>" + vraag1_2 + "</strong>";
@@ -44,3 +66,29 @@ function kennis1() {
     document.getElementById("vraag10").innerHTML = "<strong>" + vraag1_10 + "</strong>";
     document.getElementById("antwoord10").innerHTML = antwoord1_10;
 };
+
+function kennis2() {
+    document.getElementById("titelKennis").innerHTML = "Kennistoets 1.03"
+
+    document.getElementById("vraag1").innerHTML = "<strong>" + vraag2_1 + "</strong>";
+    document.getElementById("antwoord1").innerHTML = antwoord2_1;
+    document.getElementById("vraag2").innerHTML = "<strong>" + vraag2_2 + "</strong>";
+    document.getElementById("antwoord2").innerHTML = antwoord2_2;
+    document.getElementById("vraag3").innerHTML = "<strong>" + vraag2_3 + "</strong>";
+    document.getElementById("antwoord3").innerHTML = antwoord2_3;
+    document.getElementById("vraag4").innerHTML = "<strong>" + vraag2_4 + "</strong>";
+    document.getElementById("antwoord4").innerHTML = antwoord2_4;
+    document.getElementById("vraag5").innerHTML = "<strong>" + vraag2_5 + "</strong>";
+    document.getElementById("antwoord5").innerHTML = antwoord2_5;
+    document.getElementById("vraag6").innerHTML = "<strong>" + vraag2_6 + "</strong>";
+    document.getElementById("antwoord6").innerHTML = antwoord2_6;
+    document.getElementById("vraag7").innerHTML = "<strong>" + vraag2_7 + "</strong>";
+    document.getElementById("antwoord7").innerHTML = antwoord2_7;
+    document.getElementById("vraag8").innerHTML = "<strong>" + vraag2_8 + "</strong>";
+    document.getElementById("antwoord8").innerHTML = antwoord2_8;
+    document.getElementById("vraag9").innerHTML = "<strong>" + vraag2_9 + "</strong>";
+    document.getElementById("antwoord9").innerHTML = antwoord2_9;
+    document.getElementById("vraag10").innerHTML = "<strong>" + vraag2_10 + "</strong>";
+    document.getElementById("antwoord10").innerHTML = antwoord2_10;
+};
+
