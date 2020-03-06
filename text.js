0bd8353d422b3eb354e5e26eee11f52f64498670
[NikkivB] <[0316641@student.rocvantwente.nl]>
2020-03-06 12:25:43 +0100

    Create text.js

---
+++ text.js
@@ -0,0 +1,4 @@
+function converting(){
+    var input = document.getElementById("inputText").value;
+    var output = document.getElementById("outputText").value = input.split("go\n").join("\nGO\n");
+}
