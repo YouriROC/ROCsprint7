b93b6314f1e614af7cad3ed6a65e2c39820b8863
Ronan <0320293@student.rocvantwente.nl>
2020-03-10 10:07:22 +0100

    check check

---
+++ groepssite/ronanlabs.js
@@ -0,0 +1,12 @@
+function lab2() {
+    document.getElementById("code").src='Ronan/lab 2.png';
+};
+function lab3() {
+    document.getElementById("code").src='Ronan/lab 3.png';
+};
+function lab5() {
+    document.getElementById("code").src='Ronan/lab 5.png';
+};
+function lab6() {
+    document.getElementById("code").src='Ronan/lab 6.png';
+};
